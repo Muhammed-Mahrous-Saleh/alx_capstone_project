284599fbe9775197970e38862b8a6561dd3bbe73
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-10-13 01:29:23 +0300

    subtask styles

--- static/js/task.js
+++ static/js/task.js
@@ -11,22 +11,11 @@ document.getElementById('add_subtask').addEventListener('click', function () {
     subtaskIdInput.value = '0';
     newSubtaskDiv.appendChild(subtaskIdInput);
 
-    var titleLabel = document.createElement('label');
-    titleLabel.innerText = 'Title:';
-    titleLabel.htmlFor = 'subtask_title_' + subtaskCount;
-    newSubtaskDiv.appendChild(titleLabel);
-
     var titleInput = document.createElement('input');
     titleInput.type = 'text';
     titleInput.name = 'subtask_title_' + subtaskCount;
     titleInput.required = true;
     newSubtaskDiv.appendChild(titleInput);
-    newSubtaskDiv.appendChild(document.createElement('br'));
-
-    var statusLabel = document.createElement('label');
-    statusLabel.innerText = 'Status:';
-    statusLabel.htmlFor = 'subtask_status_' + subtaskCount;
-    newSubtaskDiv.appendChild(statusLabel);
 
     var statusInput = document.createElement('select');
     statusInput.name = 'subtask_status_' + subtaskCount;
@@ -37,18 +26,11 @@ document.getElementById('add_subtask').addEventListener('click', function () {
         statusInput.appendChild(option);
     });
     newSubtaskDiv.appendChild(statusInput);
-    newSubtaskDiv.appendChild(document.createElement('br'));
-
-    var dueDateLabel = document.createElement('label');
-    dueDateLabel.innerText = 'Due Date:';
-    dueDateLabel.htmlFor = 'subtask_due_date_' + subtaskCount;
-    newSubtaskDiv.appendChild(dueDateLabel);
 
     var dueDateInput = document.createElement('input');
     dueDateInput.type = 'date';
     dueDateInput.name = 'subtask_due_date_' + subtaskCount;
     newSubtaskDiv.appendChild(dueDateInput);
-    newSubtaskDiv.appendChild(document.createElement('br'));
 
     var deleteButton = document.createElement('button');
     deleteButton.type = 'button';
@@ -66,12 +48,9 @@ document.getElementById('add_subtask').addEventListener('click', function () {
 
 document.querySelectorAll('.delete_subtask').forEach(function (deleteButton) {
     deleteButton.addEventListener('click', function () {
-        deleteButton.parentElement.remove();
+        let confirm_delete = confirm('Are you sure you want to delete this subtask?');
+        if (confirm_delete) {
+            deleteButton.parentElement.remove()
+        }
     });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log(new Date().getDate())
-    
-    console.log(document.getElementById('start_date').value)
 });
